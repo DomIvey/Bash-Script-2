6aa65e4205caa88c8616ebde1d0ed4c98faeabdb
DomIvey <140740841+DomIvey@users.noreply.github.com>
2023-09-15 19:50:54 -0500

    Create buildscript2.sh

---
+++ buildscript2.sh
@@ -0,0 +1,19 @@
+Create a Bash script to SSH into a server
+
+#!/bin/bash
+
+#Have the user select ssh or scp echo "Select '1' for ssh or '2' for scp"
+
+read x
+
+#If the user chooses option 1, have them enter IP address if [ "$x" = "1" ] then echo "Please enter your IP address" read ip_address echo "Please enter your username" read username
+
+fi
+
+#If the user chooses option 2, have them enter direction of copy if [ "$x" = "2" ] then echo "Please enter the direction of copy" read direction of copy
+
+fi
+
+#cd . ssh #cat .ssh
+
+Exit
